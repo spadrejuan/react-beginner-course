263eaebd51d2a7d18d32eb9775dfde4fa75d4e2e
spadrejuan <shaunpadrejuan@gmail.com>
2024-03-17 13:03:16 +0800

    Added delete function and refactored Todo list to separate component

--- react-todo-app/src/App.tsx
+++ react-todo-app/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { dummyData } from "./data/todos"
-import TodoListItem from "./components/TodoListItem"
 import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState(dummyData)
@@ -24,20 +24,19 @@ function App() {
       },
     ]);
   }
+
+  function deleteTodo(id: number){
+    setTodos((prevTodos) => prevTodos.filter(todo => todo.id !== id))
+  }
+  
   return (
-    <main className="py-10 h-screen space-y-5">
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">
         Your TODOs
       </h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
         <AddTodoForm onSubmit={addTodo}/>
-        <div className="space-y-2">
-          {
-            todos.map((todo) => (
-              <TodoListItem key={todo.id} todo={todo} onCompletedChange={setTodoCompleted}/>
-            ))
-          }
-        </div>
+        <TodoList todos={todos} onCompletedChange={setTodoCompleted} onDelete={deleteTodo}/>
       </div>
     </main>
   )
--- react-todo-app/src/components/TodoListItem.tsx
+++ react-todo-app/src/components/TodoListItem.tsx
@@ -1,14 +1,16 @@
+import { Trash2 } from "lucide-react";
 import { Todo } from "../types/todo";
 
 interface TodoListItemProps {
     todo: Todo;
     onCompletedChange: (id: number, completed: boolean) => void
+    onDelete: (id: number) => void;
 }
 
-export default function TodoListItem({todo, onCompletedChange}: TodoListItemProps) {
+export default function TodoListItem({todo, onCompletedChange, onDelete}: TodoListItemProps) {
     return(
-        <div>
-            <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-slate-50">
+        <div className="flex items-center gap-1">
+            <label className="grow flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-slate-50">
                 <input type="checkbox" checked={todo.completed} 
                 onChange={(e) => onCompletedChange(todo.id, e.target.checked)} className="scale-125" 
                 />
@@ -16,6 +18,11 @@ export default function TodoListItem({todo, onCompletedChange}: TodoListItemProp
                     {todo.title}
                 </span>
             </label>
+            <button className="p-2" onClick={() => onDelete(todo.id)}>
+                <Trash2 size={20} className="text-gray-500">
+
+                </Trash2>
+            </button>
         </div>
     )
 }

e1bf83c9498353603bbc806688530548e78cac11
spadrejuan <shaunpadrejuan@gmail.com>
2024-03-17 11:55:17 +0800

    Added dummy todos and added styling

--- react-todo-app/src/App.tsx
+++ react-todo-app/src/App.tsx
@@ -1,9 +1,22 @@
+import { dummyData } from "./data/todos"
+
 function App() {
   return (
     <main className="py-10 h-screen">
       <h1 className="font-bold text-3xl text-center">
         Your TODOs
       </h1>
+      <div className="max-w-lg mx-auto">
+        <div className="space-y-2">
+          {
+            dummyData.map(todo => (
+              <p key = {todo.id} className="text-lg">
+                {todo.title}
+              </p>
+            ))
+          }
+        </div>
+      </div>
     </main>
   )
 }
---
+++ react-todo-app/src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "Sample Task #1",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Sample Task #2",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Sample Task #3",
+        completed: false,
+    }
+]
